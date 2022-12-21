ca636b6795418ac95cb8cde560a31eea2ce8d02e
Daniel Martinez <daniellhf@icloud.com>
2022-12-20 21:50:06 -0500

    upped the temp

--- api/index.js
+++ api/index.js
@@ -24,7 +24,7 @@ async function request(req) {
     model: "text-davinci-003",
 	  prompt: req.prompt,
 	  max_tokens: 256,
-	  temperature: 0.5,
+	  temperature: 0.8,
   });
   return { 'result': completion.data.choices[0].text };
 }
