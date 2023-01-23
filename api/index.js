154ccd9ed2a24902d7c980283d4d4b450d897b2d
Daniel Martinez <daniellhf@icloud.com>
2023-01-22 20:22:03 -0500

    initial commit

--- api/index.js
+++ api/index.js
@@ -33,24 +33,17 @@ async function request(req) {
 	// console.log(req)
   const completion = await openai.createCompletion({
     model: "text-davinci-003",
-	  prompt: req.prompt,
-	  max_tokens: 256,
-	  temperature: 0.8,
+  prompt: req.prompt,
+  temperature: 0.9,
+  max_tokens: 150,
+  top_p: 1,
+  frequency_penalty: 0,
+  presence_penalty: 0.6,
+  stop: [" Human:", " AI:"],
   });
   return { 'result': completion.data.choices[0].text };
 }
 
-function generatePrompt(animal) {
-  const capitalizedAnimal =
-    animal[0].toUpperCase() + animal.slice(1).toLowerCase();
-  return `Suggest three names for an animal that is a superhero.
-Animal: Cat
-Names: Captain Sharpclaw, Agent Fluffball, The Incredible Feline
-Animal: Dog
-Names: Ruff the Protector, Wonder Canine, Sir Barks-a-Lot
-Animal: ${capitalizedAnimal}
-Names:`;
-}
 
 app.get('/api', (req, res) => {
 	res.send("hi")
