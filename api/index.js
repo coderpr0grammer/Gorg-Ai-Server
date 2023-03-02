cdf46ec2146636834b294c78a8f33e19fb95a147
coderpr0grammer <daniellhf@icloud.com>
2023-03-02 11:50:22 -0500

    Update index.js

--- api/index.js
+++ api/index.js
@@ -98,7 +98,7 @@ app.get("/api", (req, res) => {
   let output = null;
 
 
-  request({ name: req.query.name, messages: testMessages })
+  request({ name: req.query.name, messages: [{role: 'user', content: req.query.prompt}] })
     .then((result) => {
       res.json(result);
     })
