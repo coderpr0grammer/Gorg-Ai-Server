5aa6cce701dabfff611af8867adc1c795097853d
Daniel Martinez <daniellhf@icloud.com>
2023-03-16 15:44:47 -0400

    removed org

--- api/index.js
+++ api/index.js
@@ -15,7 +15,6 @@ app.use(bodyParser.json());
 const { Configuration, OpenAIApi } = require("openai");
 
 const configuration = new Configuration({
-  organization: "org-7EI8r48srsW3pVpQ7E7KPmy6",
   apiKey: process.env.OPENAI_API_KEY,
 });
 const openai = new OpenAIApi(configuration);
